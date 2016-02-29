afde80e6ef4dbc03417c3f686bdd2e17cb6303df
Alex Tan <tanalex1@gmail.com>
2016-02-28 16:39:12 -0800

    Created model for Account; moved models to App\Models folder. Changed some database migrations to fix migration errors

---
+++ app/Models/Account.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
+
+class Account extends Authenticatable
+{
+    /**
+     * This will enable the relation with Role and add the following methods roles(), hasRole($name), 
+     * can($permission), and ability($roles, $permissions, $options) within your User model.
+     * 
+     */
+    use EntrustUserTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
---
+++ database/migrations/2016_02_20_022401_create_table_transportation.php~Updated upstream
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTransportation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transportation', function (Blueprint $table) {
+          $table->increments('id');
+          $table->integer('capacity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+      Schema::drop('transportation');
+      DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+       }
+}
