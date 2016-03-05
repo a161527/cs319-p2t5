3520f77f5f103028a426604c3f72d6fa821597d5
Ryan <ryan.io.93@gmail.com>
2016-03-04 22:03:58 -0800

    Added date picker library

--- public/protractor_tests/protractor.config.js
+++ public/protractor_tests/protractor.config.js
@@ -5,6 +5,7 @@ exports.config = {
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
+    // 'browserName': 'firefox'
   },
 
   // Framework to use. Jasmine is recommended.
