d5b0ce289b0dc0ac1d35ec363fd870c7861526dd
Shehryar Awan <GOhDi55mDream@dhcp-128-189-255-131.ubcsecure.wireless.ubc.ca>
2016-03-17 10:22:43 -0700

    Fixed up the events controller.

--- app/Http/Controllers/Events.php
+++ app/Http/Controllers/Events.php
@@ -17,21 +17,21 @@ use Config;
 
 class Events extends Controller
 {
-  public function __construct() {
-      //Allow info requests without a token.  May need to do extra
-      //auth stuff if they want detailed info, but right now we don't
+   public function __construct() {
+       //Allow info requests without a token.  May need to do extra
+       //auth stuff if they want detailed info, but right now we don't
       //make that distinction
-      $this->middleware('jwt.auth');
-      $this->middleware('jwt.check');
-  }
+       $this->middleware('jwt.auth');
+       $this->middleware('jwt.check');
+   }
 
     /**
      * Display a listing of all events in the database.
      * @return Response
      */
     public function index($id = null) {
-      if ($id == null) {return Event::orderBy('id', 'asc')->get();}
-      else {return $this->show($id);}
+       if ($id == null) {return Event::orderBy('id', 'asc')->get();}
+        else {return $this->show($id);}
     }
 
     /**
@@ -39,9 +39,9 @@ class Events extends Controller
      * @return Response
      */
     public function getEventByConferenceID($conferenceID) {
-      $event = Event::where('conferenceID',$conferenceID)->get();
-      if (count($event) == 0) {return response("No events for conferenceID {$conferenceID}.", 404);}
-      return $event;
+       $event = Event::where('conferenceID',$conferenceID)->get();
+       if (count($event) == 0) {return response("No events for conferenceID {$conferenceID}.", 404);}
+       return $event;
     }
 
     /**
@@ -50,26 +50,26 @@ class Events extends Controller
      * @return Response
      */
     public function store(Request $request, $confId) {
-      if (!Entrust::can(PermissionNames::ConferenceEventCreate($confId))) {return response("not found", 403);}
+       if (!Entrust::can(PermissionNames::ConferenceEventCreate($confId))) {return response("not found", 403);}
 
-      return DB::transaction(function () use ($request) {
-      $event = new Event;
-      $event->eventName = $request->input('eventName');
-      $event->date = $request->input('date');
-      $event->location = $request->input('location');
-      $event->startTime = $request->input('startTime');
-      $event->endTime = $request->input('endTime');
-      $event->capacity = $request->input('capacity');
-      $event->description = $request->input('description');
-      $event->conferenceID = $request->input('conferenceID');
-      $event->save();
+       return DB::transaction(function () use ($request) {
+         $event = new Event;
+         $event->eventName = $request->input('eventName');
+         $event->date = $request->input('date');
+         $event->location = $request->input('location');
+         $event->startTime = $request->input('startTime');
+         $event->endTime = $request->input('endTime');
+         $event->capacity = $request->input('capacity');
+         $event->description = $request->input('description');
+         $event->conferenceID = $request->input('conferenceID');
+         $event->save();
 
-      $role = RoleCreate::EventManager($event->id);
-      $user = Auth::user();
-      $user->attachRole($role);
+         $role = RoleCreate::EventManager($event->id);
+         $user = Auth::user();
+         $user->attachRole($role);
 
-      return response()->json(['id' => $event->id]);
-      });
+         return response()->json(['id' => $event->id]);
+       });
     }
 
     /**
@@ -78,9 +78,9 @@ class Events extends Controller
      * @return Response
      */
     public function show($id) {
-     $event = Event::find($id);
-     if (count($event) == 0) {return response("No event for id {$id}.", 404);}
-     return $event;
+      $event = Event::find($id);
+      if (count($event) == 0) {return response("No event for id {$id}.", 404);}
+      return $event;
     }
 
     /**
@@ -90,22 +90,22 @@ class Events extends Controller
      * @return Response
      */
     public function update(Request $request, $id) {
-      if (!Entrust::can(PermissionNames::EventInfoEdit($id))) {return response("", 403);}
+       if (!Entrust::can(PermissionNames::EventInfoEdit($id))) {return response("", 403);}
 
-      $event = Event::find($id);
-      if (is_null($event)) {return response("No event for id {$id}.", 404);}
+       $event = Event::find($id);
+       if (is_null($event)) {return response("No event for id {$id}.", 404);}
 
-      $event->eventName = $request->input('eventName');
-      $event->date = $request->input('date');
-      $event->location = $request->input('location');
-      $event->startTime = $request->input('startTime');
-      $event->endTime = $request->input('endTime');
-      $event->capacity = $request->input('capacity');
-      $event->description = $request->input('description');
-      $event->conferenceID = $request->input('conferenceID');
-      $event->save();
+       $event->eventName = $request->input('eventName');
+       $event->date = $request->input('date');
+       $event->location = $request->input('location');
+       $event->startTime = $request->input('startTime');
+       $event->endTime = $request->input('endTime');
+       $event->capacity = $request->input('capacity');
+       $event->description = $request->input('description');
+       $event->conferenceID = $request->input('conferenceID');
+       $event->save();
 
-      return response()->json(['id' => $event->id]);
+       return response()->json(['id' => $event->id]);
     }
 
     /**
@@ -114,12 +114,12 @@ class Events extends Controller
      * @return Response
      */
     public function destroy($id) {
-      if (!Entrust::hasRole(RoleNames::EventManager($id))) {return response("", 403);}
+       if (!Entrust::hasRole(RoleNames::EventManager($id))) {return response("", 403);}
 
-      $event = Event::find($id);
-      if (is_null($event)) {return response("No event for id {$id}.", 404);}
+       $event = Event::find($id);
+       if (is_null($event)) {return response("No event for id {$id}.", 404);}
 
-      $event->delete();
-      return response()->json(['id' => $event->id]);
+       $event->delete();
+       return response()->json(['id' => $event->id]);
     }
 }
