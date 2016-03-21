beb3da4bd4ff9b979f89795aed4c2d52b482660e
Shehryar Awan <GOhDi55mDream@dhcp-128-189-252-25.ubcsecure.wireless.ubc.ca>
2016-03-21 10:38:52 -0700

    events controller minor update.

--- app/Http/Controllers/Events.php
+++ app/Http/Controllers/Events.php
@@ -178,7 +178,7 @@ class Events extends Controller {
         $userEvent->eventID = $id;
         $userEvent->save();
 
-        return response()->json(['User Resistered' => $userEvent->id]);
+        return response()->json(['id' => $userEvent->id]);
     }
 
 }
