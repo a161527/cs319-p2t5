9a251caddc71645d5f6bab849b54cf4629472da1
Alex Tan <tanalex1@gmail.com>
2016-03-24 23:43:26 -0700

    Updated Inventory and UserInventory models

--- app/Inventory.php
+++ app/Inventory.php
@@ -8,4 +8,11 @@ class Inventory extends Model
 {
     protected $table = 'inventory';
 
+    /**
+     *	Get the conference associated with the inventory entry
+     */
+    public function conference()
+    {
+    	return $this->hasOne('App\Conference', 'conferenceID');
+    }
 }
