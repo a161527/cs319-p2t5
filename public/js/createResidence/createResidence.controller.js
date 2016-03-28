513139e6dab6ebb0050ee737593b3ccf7ff9da4e
Nikki Martin <nikjean.martin@gmail.com>
2016-03-27 17:40:54 -0700

    create multiple residences

--- public/js/createResidence/createResidence.controller.js
+++ public/js/createResidence/createResidence.controller.js
@@ -22,8 +22,7 @@
 						residenceInfo.push(res)
 					})
 
-					// TODO: residence creation endpoint needs to be updated to take in multiple objects
-					ajax.serviceCall('Creating residences...', 'post', 'api/conferences/' + $stateParams.cid + '/residences', residenceInfo[0]).then(function(resData) {
+					ajax.serviceCall('Creating residences...', 'post', 'api/conferences/' + $stateParams.cid + '/residences', residenceInfo).then(function(resData) {
 
 						$state.go('dashboard.conferences.manage', {'cid': $stateParams.cid}, {reload: true})
 
