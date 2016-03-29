41b440fa62ce0da92adb8a3790bcbb21f8617416
Brandon Sanderson <bas.bsanders@gmail.com>
2016-03-28 22:00:54 -0700

    Finish off inventory edit/patch endpoint

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -125,7 +125,6 @@ Route::group(['prefix' => 'api'], function()
         Route::get('/unapproved', 'InventoryController@unapproved');
         Route::get('/approved', 'InventoryController@approved');
         Route::post('/', 'InventoryController@addItem');
-        Route::put('/', 'InventoryController@addItem');
         Route::post('/reserve', 'InventoryController@reserveItem');
 
         Route::patch('/{itemId}', 'InventoryController@editItem');
