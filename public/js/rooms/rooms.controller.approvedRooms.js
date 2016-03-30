046ec3a26a80155fa1b7069839c6d42bd9a6a97b
Ryan <ryan.io.93@gmail.com>
2016-03-30 01:45:29 -0700

    Inventory request remove setup and hide assign transportation when none

--- public/js/rooms/rooms.controller.approvedRooms.js
+++ public/js/rooms/rooms.controller.approvedRooms.js
@@ -2,12 +2,20 @@
 	'use strict'
 
 	angular.module('rooms')
-		.controller('ApprovedRoomsCtrl', function($scope, $state, assignedRooms) {
+		.controller('ApprovedRoomsCtrl', function($scope, $state, $stateParams, $http, assignedRooms) {
 
 			$scope.roomsAssigned = assignedRooms.data
 			
 			$scope.back = function() {
 				$state.go('dashboard.conferences.manage.room-allocate.1')
+			}
+
+			$scope.remove = function(id) {
+				$http.delete('api/conferences/' + $stateParams.cid + '/residences/' + id).then(function(resData) {
+					$state.reload()
+				}, function(resData) {
+
+				})
 			}
 
 		})
