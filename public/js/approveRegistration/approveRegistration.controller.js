d9398875d8914fd287a13aa5c35edfab5dc66b80
Ryan <ryan.io.93@gmail.com>
2016-03-30 00:35:48 -0700

    Conference reject user registration

--- public/js/approveRegistration/approveRegistration.controller.js
+++ public/js/approveRegistration/approveRegistration.controller.js
@@ -6,6 +6,7 @@
 
 			$scope.state1 = true
 			$scope.state2 = !$scope.state1
+			$scope.rejectButtonTxt = 'Reject'
 
 			$scope.unapprovedList = unapproved
 
@@ -20,8 +21,6 @@
 					$state.reload()
 
 				}, function(resData) {
-					
-
 
 				})
 			}
@@ -30,11 +29,22 @@
 				$state.go('dashboard.conferences.manage.approve-registration.2')
 			}
 
+			$scope.reject = function(id) {
+				$http.delete('api/conferences/' + $stateParams.cid + '/register/' + id).then(function(resData) {
+					
+					$state.reload()
+
+				}, function(resData) {
+					console.log(resData)
+				})
+			}
+
 		})
 
-		.controller('conferenceApprovedUsersCtrl', function($scope, $stateParams, $state, approved) {
+		.controller('conferenceApprovedUsersCtrl', function($scope, $stateParams, $state, $http, approved) {
 
 			$scope.unapprovedList = approved.data
+			$scope.rejectButtonTxt = 'Remove'
 
 			angular.forEach($scope.unapprovedList, function(dep){
 				dep['fullName'] = dep.user.firstName + ' ' + dep.user.lastName
@@ -45,6 +55,16 @@
 
 			$scope.back = function() {
 				$state.go('dashboard.conferences.manage.approve-registration.1')
+			}
+
+			$scope.reject = function(id) {
+				$http.delete('api/conferences/' + $stateParams.cid + '/register/' + id).then(function(resData) {
+					
+					$state.reload()
+
+				}, function(resData) {
+					console.log(resData)
+				})
 			}
 
 		})
