9b28047be8e7b7bc9a1bf2176411a0d21805c8a3
Ryan <ryan.io.93@gmail.com>
2016-03-29 22:39:44 -0700

    Retrieve all users from flights transportation assignment

--- public/js/assignTransportation/assignTransportation.controller.js
+++ public/js/assignTransportation/assignTransportation.controller.js
@@ -7,14 +7,31 @@
 			$scope.flights = []
 
 			angular.forEach(users.data.flights, function(flight) {
-				flight.fullName = flight.accounts[1].users[0].firstName + ' ' + flight.accounts[1].users[0].lastName
-				$scope.flights.push(flight)
+				console.log(flight)
+				angular.forEach(flight.accounts, function(account) {
+
+					angular.forEach(account.users, function(user) {
+
+						user.fullName = user.firstName + ' ' + user.lastName
+						user.uid = user.id
+						user.airline = flight.airline
+						user.airport = flight.airport
+						user.arrivalDate = flight.arrivalDate
+						user.arrivalTime = flight.arrivalTime
+						user.flightNumber = flight.flightNumber
+
+						$scope.flights.push(user)
+
+					})
+
+				})
+
 			})
 
 			$scope.assign = function(idx) {
 				var param = {
 					name: $scope.flights[idx].fullName, 
-					id: $scope.flights[idx].id
+					id: $scope.flights[idx].userconferenceID
 				}
 
 				$state.go('dashboard.conferences.manage.assign-transportation.2', {user: param})
@@ -22,17 +39,22 @@
 
 		})
 
-		.controller('assignTransportationCtrl', function($scope, $stateParams, $http, transport) {
+		.controller('assignTransportationCtrl', function($scope, $stateParams, $http, $state, transport, modal) {
 
 			$scope.transportation = transport.data.transportation
 			$scope.user = $stateParams.user
 
 			$scope.assign = function() {
-				var route = 'api/conferences/' + $stateParams.cid + '/transportation/' + $stateParams.user.id + '/assign'
+				console.log($scope.user.id)
+				var route = 'api/conferences/' + $stateParams.cid + '/transportation/' + $scope.user.id + '/assign'
 				$http.post(route).then(function(resData) {
 
 					$state.go('dashboard.conferences.manage.assign-transportation.1')
 
+				}, function(resData) {
+					modal.open('Error: ' + resData.data.message, function() {
+						$state.go('dashboard.conferences.manage.assign-transportation.1')
+					})
 				})
 			}
 
